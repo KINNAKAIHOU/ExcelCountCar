a8a4020e42a769e5c26390a4c6b6a10355534d64
KinnakaIhou <2570719876@qq.com>
2023-09-14 11:48:15 +0800

    Excel车辆数据统计

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,8 +10,6 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        StopWatch stopWatch = new StopWatch();
-        stopWatch.start("判断是否施封");
         Excel3 excel = new Excel3();
         excel.useExcel();
     }
